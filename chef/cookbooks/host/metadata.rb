edac83809559effcdf9a8e1fdbe88b09b7e95569
Evan Broder <evan@ebroder.net>
2022-03-24 11:22:39 -0700

    Start on Linux bootstrapping
    This gets to the point of chezmoi applying, but some manual setup is
    still required.

--- chef/cookbooks/host/metadata.rb
+++ chef/cookbooks/host/metadata.rb
@@ -1,3 +1,4 @@
 name 'host'
+# TODO: install and set shell to zsh
 depends 'golang'
 depends 'nvm'
--- run_before_00_chef.sh
+++ run_before_00_chef.sh
@@ -2,6 +2,14 @@
 
 set -euxo pipefail
 
+if ! which chezmoi; then
+  PATH="$PATH:$HOME/bin"
+  if ! which chezmoi; then
+    echo "chezmoi not found"
+    exit 1
+  fi
+fi
+
 if [ "$(uname)" = "Darwin" ]; then
   if ! xcode-select -p &>/dev/null; then
     sudo xcodebuild -license accept
@@ -22,9 +30,11 @@ if [ "$(uname)" = "Darwin" ]; then
   if ! which chef-solo >/dev/null; then
     brew install chef/chef/chef-infra-client
   fi
+elif [ "$(uname)" = "Linux" ]; then
+  if ! which chef-solo > /dev/null; then
+    curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -P chef
+  fi
 fi
-
-# TODO: Linux install
 
 cd "$(chezmoi source-path)/chef"
 CHEF_LICENSE=accept-silent chef-solo -c solo.rb -j attributes.json
